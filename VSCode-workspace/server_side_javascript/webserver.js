11db606f0c5e11e478951d1da269cd47b8f85723
daganex <daganex@naver.com>
2020-05-22 18:28:17 +0900

    테스트 18:28

---
+++ VSCode-workspace/server_side_javascript/webserver.js
@@ -0,0 +1,13 @@
+/**  WEB SERVER - NODE JS 에서 제공하는 Http 모듈 */ 
+// const 상수
+const http = require('http');
+ 
+const hostname = '127.0.0.1';
+const port = 1337;
+ 
+http.createServer((req, res) => {
+ res.writeHead(200, { 'Content-Type': 'text/plain' });
+ res.end('Hello World\n');
+}).listen(port, hostname, () => {
+ console.log(`Server running at http://${hostname}:${port}/`);
+});
