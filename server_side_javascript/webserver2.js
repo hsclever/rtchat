9f261207d9c0977e384a1b244716a997f76735e0
daganex <daganex@naver.com>
2020-05-22 17:30:39 +0900

    2020-05-22 첫 커밋

---
+++ server_side_javascript/webserver2.js
@@ -0,0 +1,15 @@
+/**  WEB SERVER - NODE JS 에서 제공하는 Http 모듈 */ 
+// const 상수
+const http = require('http');
+ 
+const hostname = '127.0.0.1';
+const port = 1337;
+
+var server = http.createServer(function(req, res){
+     res.writeHead(200, { 'Content-Type': 'text/plain' });
+     res.end('Hello World\n');
+});
+
+server.listen(port , hostname, function(){
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
